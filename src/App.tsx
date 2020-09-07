802a175a9aba3c65058aa5aa772b59eac98bd8d5
mahesh <mahesh@blocklogy.org>
2020-09-07 19:14:28 +0530

    first push

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Navbar } from './components/Navbar';
+import { Footer } from './components/Footer';
+import { Homepage } from './containers/Homepage';
+import './App.css';
+ 
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+        <Router>
+          <Switch>
+            <Route exact path="/" component={Homepage} />
+           </Switch>
+        </Router>
+
+        <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,8 @@
+import React, { useContext } from 'react';
+
+ 
+export const  Navbar = () => {
+	return <header>
+	<p>Here comes navbar</p>
+	</header>;
+}
